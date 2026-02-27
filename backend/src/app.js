b5faae4fdcdd09f0c55c56a864a895764df118bf
Selamawit Shimeles <selamshimeles9@gmail.com>
2026-02-28 02:44:26 +0300

    setting up environment and connect to database

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'API is working!' });
+});
+
+export default app;
---
+++ front-end/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+        <h1>Hello split bill</h1>
+    </div>
+  )
+}
+
+export default App
